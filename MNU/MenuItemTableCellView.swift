7217042fb37963f5a903ba25656e0c6d17b9bfaf
smittytone <smittytone@icloud.com>
2019-07-24 11:34:45 +1000

    Add a custom table cell view for the configure window table

---
+++ MNU/MenuItemTableCellView.swift
@@ -0,0 +1,18 @@
+
+//  MNUitemTableCellView.swift
+//  MNU
+//
+//  Created by Tony Smith on 24/07/2019.
+//  Copyright © 2019 Tony Smith. All rights reserved.
+
+
+import Cocoa
+
+class MenuItemTableCellView: NSTableCellView {
+
+    // This table cell view simply has a couple of extra items we want to make accessible
+    
+    @IBOutlet weak var title: NSTextField!
+    @IBOutlet weak var button: MenuItemTableCellButton!
+    
+}
