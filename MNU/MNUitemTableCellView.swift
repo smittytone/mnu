ebd9a19aa21bfeced3f056216fe042993182b0e1
smittytone <smittytone@icloud.com>
2019-07-24 10:04:37 +1000

    Add custom table cell view

---
+++ MNU/MNUitemTableCellView.swift
@@ -0,0 +1,24 @@
+//
+//  AddUserItemTableCellView.swift
+//  MNU
+//
+//  Created by Tony Smith on 24/07/2019.
+//  Copyright © 2019 Tony Smith. All rights reserved.
+//
+
+import Cocoa
+
+class MNUitemTableCellView: NSTableCellView {
+
+    @IBOutlet weak var cellTitle: NSTextField!
+    @IBOutlet weak var cellButton: NSButton!
+
+    
+    override func draw(_ dirtyRect: NSRect) {
+
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
