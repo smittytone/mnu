f09b4b53166e7ffbbb6473f6b47d70677a8e6917
smittytone <smittytone@icloud.com>
2019-07-24 11:35:23 +1000

    Subclass NSButton so it can store a MNU item reference

---
+++ MNU/MenuItemTableCellButton.swift
@@ -0,0 +1,18 @@
+
+//  MenuItemTableCellButton.swift
+//  MNU
+//
+//  Created by Tony Smith on 24/07/2019.
+//  Copyright © 2019 Tony Smith. All rights reserved.
+
+
+import Cocoa
+
+
+class MenuItemTableCellButton: NSButton {
+
+    // Add a property to point to the button's parent menu item
+
+    var menuItem: MNUitem? = nil
+    
+}
