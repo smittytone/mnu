617e65f6e61b4f00c3c25a77e63e1d0a50eef363
smittytone <smittytone@icloud.com>
2020-01-14 12:21:30 +0000

    Update copyright dates

--- MNU/Constants.swift
+++ MNU/Constants.swift
@@ -4,7 +4,7 @@
     MNU
 
     Created by Tony Smith on 5/07/2019.
-    Copyright © 2019 Tony Smith. All rights reserved.
+    Copyright © 2019-20 Tony Smith. All rights reserved.
 
     MIT License
     Permission is hereby granted, free of charge, to any person obtaining a copy
