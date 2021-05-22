10a6843da65635e9f91a2aefb07cdc84c8fe0336
smittytone <smittytone@icloud.com>
2021-05-22 14:57:31 +0100

    Sync repo with script in use

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,35 +1,45 @@
-#!/bin/bash
+#!/bin/sh
 # This postinstall script is run by macOS' .pkg installer
 
 # If MNU is running, close it and then launch the newly
 # installed version. If it is not running, do not launch it
-# Version 1.0.4
+# Version 1.1.3
 
+# Set the target
 app="/Applications/MNU.app"
 
+# FROM 1.1.0
+# Add logging
+log=~/.mnulog
+if ! [ -e "$log" ]; then
+    touch "$log"
+fi
+
 # Get MNU running instance -- will be empty if MNU not running
-pid=$(ps -A | grep -m1 MNU | grep -v grep)
+# FROM 1.1.0 -- use pgrep
+old_pid=$(pgrep -x MNU)
 
 # Check process is running, and kill if it is
 # This will be the case if $pid has a non-zero length
-if [ -n "$pid" ]; then
+if [ -n "$old_pid" ]; then
     # Get the actual PID and use it to kill the process
-    pid=$(echo "$pid" | awk {'print$1'})
-    kill "$pid"
-    syslog -s -l error "Existing MNU version closed"
+    # pid=$(echo "$pid" | awk {'print$1'})
+    kill "$old_pid"
+    echo "$(date) Existing MNU ($old_pid) instance closed down" >> "$log"
 
     # Open the newly installed version if we can
     if [ -e "$app" ]; then
-        if open "$app"; then
-            syslog -s -l error "New MNU version started"
+        if open -a "$app" ; then
+            new_pid=$(pgrep -x MNU)
+            echo "$(date) New MNU ($new_pid) instance started" >> "$log"
         else
-            syslog -s -l error "MNU not launched"
+            echo "$(date) New MNU instance not started: ${?}" >> "$log"
         fi
     else
-        syslog -s -l error "MNU not found in /Applications"
+        echo "$(date) MNU not found in /Applications" >> "$log"
     fi
 else
-    syslog -s -l error "MNU not running"
+    echo "$(date) MNU not running -- please launch manually" >> "$log"
 fi
 
 exit 0
